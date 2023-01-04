07150081077f4ef439d3fcca0b4ec87df6a77e34
Jerunnon <simon-info@gmx.net>
2023-01-04 18:20:08 +0100

    added className property

--- league-challenges/components/layouts/flexcontainer.tsx
+++ league-challenges/components/layouts/flexcontainer.tsx
@@ -2,13 +2,14 @@ type IContainer = {
     direction: string,
     wrap: boolean,
     alignItems: string
-    justifyContent: string
+    justifyContent: string,
+    className: string
     children: React.ReactNode;
 }
 
-export const FlexContainer = ({ direction, wrap, alignItems, justifyContent, children}: IContainer):JSX.Element => {
+export const FlexContainer = ({ direction, wrap, alignItems, justifyContent, className, children}: IContainer):JSX.Element => {
     return (
-        <div className={`container flex flex-${direction} ${wrap ? 'flex-wrap' : 'flex-nowrap'} items-${alignItems} justify-${justifyContent} mx-auto p-4`}>
+        <div className={`container flex flex-${direction} ${wrap ? 'flex-wrap' : 'flex-nowrap'} items-${alignItems} justify-${justifyContent} ${className} mx-auto p-4`}>
             {children}
         </div>
     )
