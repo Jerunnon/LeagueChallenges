e8f89622616bcfaf5ad52133a39c9c1878061b13
Jerunnon <simon-info@gmx.net>
2023-01-04 14:37:12 +0100

    added stylings

--- league-challenges/tailwind.config.js
+++ league-challenges/tailwind.config.js
@@ -1,63 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     './app/**/*.{js,ts,jsx,tsx}',
     './page/**/*.{js,ts,jsx,tsx}',
-    './ui/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
+  plugins: [require('flowbite/plugin')],
   theme: {
     extend: {
-      // https://vercel.com/design/color
-      colors: {
-        vercel: {
-          pink: '#FF0080',
-          blue: '#0070F3',
-          cyan: '#50E3C2',
-          orange: '#F5A623',
-          violet: '#7928CA',
-        },
+      fontFamily: {
+        'sans': ['Helvetica','Roboto', ...defaultTheme.fontFamily.sans]
       },
-      keyframes: ({ theme }) => ({
-        rerender: {
-          '0%': {
-            'border-color': theme('colors.vercel.pink'),
-          },
-          '40%': {
-            'border-color': theme('colors.vercel.pink'),
-          },
-        },
-        highlight: {
-          '0%': {
-            background: theme('colors.vercel.pink'),
-            color: theme('colors.white'),
-          },
-          '40%': {
-            background: theme('colors.vercel.pink'),
-            color: theme('colors.white'),
-          },
-        },
-        shimmer: {
-          '100%': {
-            transform: 'translateX(100%)',
-          },
-        },
-        translateXReset: {
-          '100%': {
-            transform: 'translateX(0)',
-          },
-        },
-        fadeToTransparent: {
-          '0%': {
-            opacity: 1,
-          },
-          '40%': {
-            opacity: 1,
-          },
-          '100%': {
-            opacity: 0,
-          },
-        },
-      }),
+      colors: {
+        coral: '#4f5d75',
+        pine: '#519E8A',
+        sand: '#ee964b',
+        'space': {
+          DEFAULT: '#2D3142',
+          200: '#212331'
+        }
+      },
+      maxWidth: {
+        85: '85%',
+        90: '90%'
+      },
     },
   },
 };
