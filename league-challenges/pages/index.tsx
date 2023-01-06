99bbfc439acce6523924c4aec67bfecec8d8972b
Jerunnon <simon-info@gmx.net>
2023-01-06 04:22:34 +0100

    added example card

--- league-challenges/pages/index.tsx
+++ league-challenges/pages/index.tsx
@@ -1,11 +1,29 @@
-import Layout from "@/ui/layouts/main"
+import * as React from 'react';
+
+import Card from '@/ui/Card';
+
 
 function index() {
+
+  const [ mounted, setMounted ] = React.useState<Boolean>(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if(!mounted) return null;
+
   return (
-   
-    <div>index</div>
-    
+    <React.Fragment>
+      <Card width="w-96">
+        <Card.Body align="text-center">
+          <Card.Image src="https://placeimg.com/400/225/arch" alt="test" className='rounded-xl'/>
+          <Card.Heading title='CardText' />
+          <Card.Actions text='test' variant='primary' />
+        </Card.Body>
+      </Card>
+    </React.Fragment>
   )
 }
 
-export default index
+export default index;
